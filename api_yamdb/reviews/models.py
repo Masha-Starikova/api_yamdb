24bf6f21b3d83e3cbfa7c4c3329e8a43febe9550
Мария Андреевна <79227313534@yandex.ru>
2022-10-05 19:06:51 +0500

    Модель R3

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+class Comment(models.Model):
+    text = models.TextField()
+    autor = models.ForeignKey() #username автора комментария
+    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
+
+    class Meta:
+        ordering = ('-pub_date', )
+        verbose_name = 'Комментарий'
+
+    def __str__(self):
+        return self.text[:15]
+
+
+class Review(models.Model):
+    SCORE_CHOICES = zip(range(1,10), range(1,10) )
+    text = models.TextField()
+    autor = models.ForeignKey() #username автора комментария
+    score = models.IntegerField(default=1, validators=[
+            MaxValueValidator(10),
+            MinValueValidator(1)
+        ])
+    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
