6dc4a0d4796aea3c6452456bcc54b7df1152694b
Эльмира Зайнагабдинова <elmiraz1.z@yandex.ru>
2022-10-06 19:56:21 +0300

    added custom user model

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    ROLES = (
+        ('admin', 'admin'),
+        ('moderator', 'moderator'),
+        ('user', 'user')
+    )
+    role = models.CharField(max_length=20, choices=ROLES, default='user')
