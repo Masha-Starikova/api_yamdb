fa02c4376c066e13c65675c81c7274f52d840427
george <gsekhniev@gmail.com>
2022-10-07 15:40:18 +0300

    R-2

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,6 +1,49 @@
 from rest_framework import serializers
 
-from reviews.models import Comment, Review
+from reviews.models import Genre, Category, Title, Comment, Review
+
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = ('name', 'slug')
+
+
+class CategorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Category
+        fields = ('name', 'slug')
+
+
+class GenreField(serializers.SlugRelatedField):
+
+    def to_representation(self, value):
+        return GenreSerializer(value).data
+
+
+class CategoryField(serializers.SlugRelatedField):
+
+    def to_representation(self, value):
+        return CategorySerializer(value).data
+
+
+class TitleSerializer(serializers.ModelSerializer):
+    genre = GenreField(
+        many=True,
+        slug_field='slug',
+        queryset=Genre.objects.all()
+    )
+    category = CategoryField(
+        slug_field='slug',
+        queryset=Category.objects.all()
+    )
+    rating = serializers.IntegerField(read_only=True, required=False)
+
+    class Meta:
+        fields = '__all__'
+        model = Title
 
 
 class CommentSerializer(serializers.ModelSerializer):
--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,4 +1,25 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Genre, Category, Title
 
 admin.site.register(User)
+
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'slug')
+
+
+admin.site.register(Genre, GenreAdmin)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'slug')
+
+
+admin.site.register(Category, CategoryAdmin)
+
+
+class TitleAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'year', 'category', 'description')
+
+
+admin.site.register(Title, TitleAdmin)
