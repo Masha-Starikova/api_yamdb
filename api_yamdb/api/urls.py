83f5d0ef11419c6a1095e29f1d1b4b05a098a399
Эльмира Зайнагабдинова <elmiraz1.z@yandex.ru>
2022-10-19 20:23:54 +0300

    fixed by review

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,8 +1,8 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from api.views import (CategoryViewSet, CommentViewSet, GenreViewSet, GetToken,
-                       ReviewViewSet, Signup, TitleViewSet, UserViewSet)
+from api.views import (CategoryViewSet, CommentViewSet, GenreViewSet,
+                       ReviewViewSet, sign_up, TitleViewSet, UserViewSet, get_token)
 
 v1_router = routers.DefaultRouter()
 v1_router.register('genres', GenreViewSet, basename='genres')
@@ -22,6 +22,6 @@ v1_router.register(
 )
 urlpatterns = [
     path('v1/', include(v1_router.urls)),
-    path('v1/auth/token/', GetToken.as_view()),
-    path('v1/auth/signup/', Signup.as_view())
+    path('v1/auth/token/', get_token),
+    path('v1/auth/signup/', sign_up)
 ]
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -3,7 +3,7 @@ from django.db.models import Avg
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import permissions, status, viewsets
-from rest_framework.decorators import action
+from rest_framework.decorators import action, api_view
 from rest_framework.filters import SearchFilter
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -49,8 +49,9 @@ class UserViewSet(viewsets.ModelViewSet):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
-class Signup(APIView):
-    def post(self, request):
+@api_view(['POST'])
+def sign_up(request):
+    if request.method == "POST":
         serializer = SignupSerializer(data=request.data)
         if serializer.is_valid():
             username = serializer.data.get('username')
@@ -63,10 +64,26 @@ class Signup(APIView):
                     status=200
                 )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+ 
+# class Signup(APIView):
+#     def post(self, request):
+#         serializer = SignupSerializer(data=request.data)
+#         if serializer.is_valid():
+#             username = serializer.data.get('username')
+#             if username == 'me':
+#                 return Response(status=status.HTTP_400_BAD_REQUEST)
+#             email = serializer.data.get('email')
+#             if create_user(username, email):
+#                 return Response(
+#                     serializer.data,
+#                     status=200
+#                 )
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class GetToken(APIView):
-    def post(self, request):
+@api_view(['POST'])
+def get_token(request):
+    if request.method == "POST":
         serializer = AuthSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
             username = serializer.data.get('username')
@@ -76,6 +93,17 @@ class GetToken(APIView):
                 token = AccessToken.for_user(user)
                 return Response({'token': str(token)})
         return Response(status=status.HTTP_400_BAD_REQUEST)
+# class GetToken(APIView):
+#     def post(self, request):
+#         serializer = AuthSerializer(data=request.data)
+#         if serializer.is_valid(raise_exception=True):
+#             username = serializer.data.get('username')
+#             user = get_object_or_404(User, username=username)
+#             confirmation_code = serializer.data.get('confirmation_code')
+#             if user.check_confirmation_code(confirmation_code):
+#                 token = AccessToken.for_user(user)
+#                 return Response({'token': str(token)})
+#         return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
